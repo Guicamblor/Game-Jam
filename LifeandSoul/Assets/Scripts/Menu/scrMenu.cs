8a02a62756343d335fc5f2e9611a862c49729b3e
Mushu2 <guicamblor@gmail.com>
2019-10-20 19:43:04 -0300

    Final Part 2

---
+++ LifeandSoul/Assets/Scripts/Menu/scrMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scrMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("SalaCen");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
