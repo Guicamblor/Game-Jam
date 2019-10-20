23772c8937ddc84b54941d939855b7799cb8b578
Mushu2 <guicamblor@gmailc.om>
2019-10-19 22:55:08 -0300

    1.3.5

--- LifeandSoul/Assets/Scripts/Sala1Minigame/AI.cs
+++ LifeandSoul/Assets/Scripts/Sala1Minigame/AI.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public float startWaitTime;
+    public Transform[] moveSpots;
+
+    private int randomSpot;
+    private float waitTime;
+
     void Start()
     {
-        
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
     }
 }
